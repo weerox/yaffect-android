45aa3cb7f1c006d84d7e6c7534ed5650b558a17f
Aron Bergman <bathingrad@gmail.com>
2017-09-18 21:56:41 +0200

    Changed the MainActivity layout
    A ScrollView is now the root element

--- app/src/main/java/se/yaffect/android/MainActivity.java
+++ app/src/main/java/se/yaffect/android/MainActivity.java
@@ -4,10 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
-import java.util.ArrayList;
-
-import se.yaffect.android.view.MultichoiceAnswerView;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,12 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LinearLayout main = (LinearLayout) this.findViewById(R.id.layout_main);
-        ArrayList<String> alternatives = new ArrayList<String>();
-        alternatives.add("Test");
-        alternatives.add("Test 3");
-
-        MultichoiceAnswerView multichoiceAnswerView = new MultichoiceAnswerView(this, alternatives);
-
-        main.addView(multichoiceAnswerView);
     }
 }
