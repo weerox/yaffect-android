0d2825c48ebdac9c55416426aca0c7496c97dda3
Aron Bergman <bathingrad@gmail.com>
2018-01-29 16:00:55 +0100

    Add basic field and methods to LoginActivity

--- app/src/main/java/se/yaffect/android/LoginActivity.java
+++ app/src/main/java/se/yaffect/android/LoginActivity.java
@@ -2,12 +2,53 @@ package se.yaffect.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        buttonLogin = (Button) findViewById(R.id.button_login);
+
+        password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    login(email.getText().toString(), password.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login(email.getText().toString(), password.getText().toString());
+            }
+        });
+    }
+
+    /**
+     * Attempt to log in user using provided credentials
+     * @param email email to use for log in
+     * @param password password to use for login
+     */
+    private void login(String email, String password) {
+        // TODO: Perform login
     }
 }
