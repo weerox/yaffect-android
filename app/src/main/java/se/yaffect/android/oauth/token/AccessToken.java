b7446a43d35526986e706e8ffe0e13821345d49e
Aron Bergman <bathingrad@gmail.com>
2018-01-30 18:54:20 +0100

    Add a constructor which parses Json response

--- app/src/main/java/se/yaffect/android/oauth/token/AccessToken.java
+++ app/src/main/java/se/yaffect/android/oauth/token/AccessToken.java
@@ -1,10 +1,36 @@
 package se.yaffect.android.oauth.token;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class AccessToken {
     private String accessToken;
     private TokenType tokenType;
     private int expiresIn;
     private RefreshToken refreshToken;
+
+    public AccessToken(JSONObject response) {
+        try {
+            accessToken = response.getString("access_token");
+
+            switch (response.getString("token_type")) {
+                case "Bearer":
+                    tokenType = TokenType.BEARER;
+                    break;
+                default:
+                    tokenType = TokenType.BEARER;
+                    break;
+            }
+
+            expiresIn = response.getInt("expires_in");
+
+            if (response.has("refresh_token"))
+                refreshToken = new RefreshToken(response.getString("refresh_token"));
+
+        } catch (JSONException exception) {
+            exception.printStackTrace();
+        }
+    }
 
     public AccessToken(String accessToken, TokenType tokenType, int expiresIn) {
         this(accessToken, tokenType, expiresIn, null);
