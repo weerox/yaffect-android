6f8f8117f7e26e3ce36eed894235ce5addb81c32
Aron Bergman <bathingrad@gmail.com>
2018-01-30 20:14:58 +0100

    Add a toString method
    The method returns a BASE64 encoded string (client_id:client_secret)

--- app/src/main/java/se/yaffect/android/oauth/ClientCredentials.java
+++ app/src/main/java/se/yaffect/android/oauth/ClientCredentials.java
@@ -1,8 +1,16 @@
 package se.yaffect.android.oauth;
 
+import android.util.Base64;
+
+import java.io.UnsupportedEncodingException;
+
 public class ClientCredentials {
 
     private String clientId, clientSecret;
+
+    public ClientCredentials(String clientId) {
+        this(clientId, "");
+    }
 
     public ClientCredentials(String clientId, String clientSecret) {
         this.clientId = clientId;
@@ -15,5 +23,15 @@ public class ClientCredentials {
 
     public String getClientSecret() {
         return clientSecret;
+    }
+
+    @Override
+    public String toString() {
+        try {
+            return Base64.encodeToString((getClientId() + ":" + getClientSecret()).getBytes("UTF-8"), Base64.DEFAULT);
+        } catch (UnsupportedEncodingException exception) {
+            exception.printStackTrace();
+        }
+        return null;
     }
 }
