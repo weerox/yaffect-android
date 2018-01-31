825595fc07d5fadc1b73f8e579b3393fd944385d
Aron Bergman <bathingrad@gmail.com>
2018-01-31 09:11:52 +0100

    Add a constructor
    The constructor reads the id and secret from properties file

--- app/src/main/java/se/yaffect/android/oauth/ClientCredentials.java
+++ app/src/main/java/se/yaffect/android/oauth/ClientCredentials.java
@@ -1,12 +1,31 @@
 package se.yaffect.android.oauth;
 
+import android.content.Context;
 import android.util.Base64;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import se.yaffect.android.R;
 
 public class ClientCredentials {
 
     private String clientId, clientSecret;
+
+    public ClientCredentials(Context context) {
+        try {
+            InputStream rawResource = context.getResources().openRawResource(R.raw.app);
+            Properties properties = new Properties();
+            properties.load(rawResource);
+
+            clientId = properties.getProperty("CLIENT_ID");
+            clientSecret = properties.getProperty("CLIENT_SECRET", "");
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
 
     public ClientCredentials(String clientId) {
         this(clientId, "");
